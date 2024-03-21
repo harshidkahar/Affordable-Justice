e4b2750a5876bd1097e3f4292874017269b31315
MOHAMMED ZAHID QURESHI <zahidqureshi5104@outlook.com>
2024-03-21 13:41:34 +0530

    commit

---
+++ Starterkit/Controllers/LawyerController1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Starterkit.Controllers
+{
+	public class LawyerController1 : Controller
+	{
+		[HttpGet("lawyerProfileOverview/")]
+		public IActionResult Index()
+		{
+			return View("Views/Pages/Lawyer/lawyerProfileOverview.cshtml");
+		}
+		[HttpGet("lawyerProfileSetting/")]
+		public IActionResult lawyerProfileSetting()
+		{
+			return View("Views/Pages/Lawyer/lawyerProfileSetting.cshtml");
+		}
+
+		[HttpGet("lawyerAssignedCases/")]
+		public IActionResult LawyerAssignedCases()
+		{
+			return View("Views/Pages/Lawyer/lawyerAssignedCases.cshtml");
+		}
+
+		[HttpGet("lawyerCaseDetails/")]
+		public IActionResult LawyerCaseDetails()
+		{
+			return View("Views/Pages/Lawyer/lawyerCaseDetails.cshtml");
+		}
+
+	}
+}
