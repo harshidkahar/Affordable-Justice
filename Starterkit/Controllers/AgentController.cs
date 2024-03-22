f0789fbfe466512cfde10b11b8747179edb7e3e7
MOHAMMED ZAHID QURESHI <zahidqureshi5104@outlook.com>
2024-03-22 10:45:00 +0530

    commit

--- Starterkit/Controllers/AgentController.cs
+++ Starterkit/Controllers/AgentController.cs
@@ -16,5 +16,35 @@ namespace Starterkit.Controllers
 			return View("Views/Pages/Agent/AgentProfileSetting.cshtml");
 		}
 
+		[HttpGet("RegisteredCaseList/")]
+		public IActionResult RegisteredCaseList()
+		{
+			return View("Views/Pages/Agent/RegisteredCaseList.cshtml");
+		}
+
+		[HttpGet("AgentViewRegisteredCompanyList/")]
+		public IActionResult AgentViewRegisteredCompanyList()
+		{
+			return View("Views/Pages/Agent/AgentViewRegisteredCompanyList.cshtml");
+		}
+
+		[HttpGet("RegisteredCaseDetails/")]
+		public IActionResult RegisteredCaseDetails()
+		{
+			return View("Views/Pages/Agent/RegisteredCaseDetails.cshtml");
+		}
+
+		[HttpGet("RegisteredCompanyDetails/")]
+		public IActionResult RegisteredCompanyDetails()
+		{
+			return View("Views/Pages/Agent/RegisteredCompanyDetails.cshtml");
+		}
+
+		[HttpGet("ViewPendingCaseList/")]
+		public IActionResult ViewPendingCaseList()
+		{
+			return View("Views/Pages/Agent/ViewPendingCaseList.cshtml");
+		}
+
 	}
 }
