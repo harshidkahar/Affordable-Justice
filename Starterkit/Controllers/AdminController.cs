89e1212a548152789f4fa4ac3bc5b9e70499f9d7
MOHAMMED ZAHID QURESHI <zahidqureshi5104@gmail.com>
2024-03-24 15:58:04 +0530

    commit

---
+++ Starterkit/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Starterkit.Controllers
+{
+	public class AdminController : Controller
+	{
+
+		[HttpGet("CreateAdmin/")]
+		public IActionResult Index()
+		{
+			return View("Views/Pages/Admin/CreateAdmin.cshtml");
+		}
+
+		[HttpGet("ViewAdmin/")]
+		public IActionResult ViewAdmin()
+		{
+			return View("Views/Pages/Admin/ViewAdmin.cshtml");
+		}
+
+		[HttpGet("UpdateAdmin/")]
+		public IActionResult UpdateAdmin()
+		{
+			return View("Views/Pages/Admin/UpdateAdmin.cshtml");
+		}
+
+		[HttpGet("DeleteAdmin/")]
+		public IActionResult DeleteAdmin()
+		{
+			return View("Views/Pages/Admin/DeleteAdmin.cshtml");
+		}
+	}
+}
