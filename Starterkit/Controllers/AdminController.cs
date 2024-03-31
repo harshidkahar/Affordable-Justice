5c90a7f60f2f4d6e0480fda5fb924c378da4aa02
MOHAMMED ZAHID QURESHI <zahidqureshi5104@gmail.com>
2024-04-01 02:24:51 +0530

    commit

--- Starterkit/Controllers/AdminController.cs
+++ Starterkit/Controllers/AdminController.cs
@@ -28,5 +28,54 @@ namespace Starterkit.Controllers
 		{
 			return View("Views/Pages/Admin/DeleteAdmin.cshtml");
 		}
+
+		[HttpGet("admin/CreateAgent/")]
+		public IActionResult CreateAgent()
+		{
+			return View("Views/Pages/Admin/CreateAgent.cshtml");
+		}
+
+		[HttpGet("admin/UpdateAgent/")]
+		public IActionResult UpdateAgent()
+		{
+			return View("Views/Pages/Admin/UpdateAgent.cshtml");
+		}
+
+		[HttpGet("admin/DeleteAgent/")]
+		public IActionResult DeleteAgent()
+		{
+			return View("Views/Pages/Admin/DeleteAgent.cshtml");
+		}
+
+		[HttpGet("admin/ViewAgent/")]
+		public IActionResult ViewAgent()
+		{
+			return View("Views/Pages/Admin/ViewAgent.cshtml");
+		}
+
+		[HttpGet("admin/CreateLawyer/")]
+		public IActionResult CreateLawyer()
+		{
+			return View("Views/Pages/Admin/CreateLawyer.cshtml");
+		}
+
+		[HttpGet("admin/ViewLawyer/")]
+		public IActionResult ViewLawyer()
+		{
+			return View("Views/Pages/Admin/ViewLawyer.cshtml");
+		}
+
+		[HttpGet("admin/UpdateLawyer/")]
+		public IActionResult UpdateLawyer()
+		{
+			return View("Views/Pages/Admin/UpdateLawyer.cshtml");
+		}
+
+		[HttpGet("admin/DeleteLawyer/")]
+		public IActionResult DeleteLawyer()
+		{
+			return View("Views/Pages/Admin/DeleteLawyer.cshtml");
+		}
+
 	}
 }
